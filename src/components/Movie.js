93e4797ac3b18db2e118bcf800ecf517ea4f050a
João Paulo Matos Mendes <64609444+jotammendes@users.noreply.github.com>
2021-10-25 14:35:37 -0300

    style: fix posição de voto em listagem de filmes

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,9 +5,11 @@ const Movie = ({ movie }) => {
     console.log(movie);
     return (
         <div className={ styles.movie }>
-            <img className={ styles.poster } src={ movie.poster_path } alt={ `Poster de ${movie.title}` } />
+            <div className={ styles.poster_vote }>
+                <img className={ styles.poster } src={ movie.poster_path } alt={ `Poster de ${movie.title}` } />
+                <div className={ styles.vote }>{ movie.vote_average }</div>
+            </div>
             <h3 className={ styles.title }>{ movie.title }</h3>
-            <div className={ styles.vote }>{ movie.vote_average }</div>
         </div>
     )
 }
